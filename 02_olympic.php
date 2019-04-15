76ddf68efc19d5caabed4d88497a064418aeeb79
Eun Ju Jhong <eun01300714@gmail.com>
2019-04-16 01:22:51 +0900

    first commit

---
+++ 02_olympic.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+       <meta charset="utf-8">
+       <title>Year of olympic</title>     
+    </head>
+    <?php
+    $year1 = "1988";
+    $year2 = "2018";
+    ?>
+    <body>
+        <div class="main-contents">
+            before seoul olympic held in
+            <h1><?php echo $year1; ?></h1><br>
+            after seoul olympic will be held in
+            <h1><?php echo $year2; ?></h1><br>
+    </body>
+</html>
